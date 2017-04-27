56b9d088c2867d32852cb03675902c3fc800df3d
kou yang <kyang45@mail.csuchico.edu>
2017-04-27 13:51:07 -0700

    gallery.js last part
    after the array has been edited i had to update the element with document.getElementById(" ").innerHTML. then make the innerHTML = to the random array by using (current.name of array). only one differnece is for the picture. for picture  i had to use (src) instead of (innerHTML). and for source i had type it in ad  ("image courtesy of:" + current.form) so it will write it as  image courtesy of: restaurantschico.com or whatever the array of from is.

--- gallery.js
+++ gallery.js
@@ -1,23 +1,23 @@
 var chico=[
-  { description: "bidwell park",
-  picture:"https://www.downtownchico.com/bidwell-park.htm",
-  from:"downtownchico.com"
-  }
-  { description: "bear hole",
-  picture:"https://www.pinterest.com/pin/446067538063953238/",
-  from:"pinterest.com"
-  }
-  { description: "observatory",
-  picture:"http://robinindar.com/portfolio/public-mosaic-art/chico-observatory-night-sky-mosaic/",
+  { description: "Bidwell Park",
+  picture:"http://bidwellpark.org/wp-content/uploads/2014/04/BidwellPool.jpg",
+  from:"bidwellpark.org/"
+  },
+  { description: "Bear Hole",
+  picture:"https://s-media-cache-ak0.pinimg.com/736x/91/51/cd/9151cdacefa31f37bbd2fdcbbef89f1b.jpg",
+  from:"pinimg.com"
+  },
+  { description: "Observatory",
+  picture:"http://robinindar.com/wp-content/uploads/2015/02/observatory-06-006.jpg",
   from:"robinindar.com"
-  }
-  { description: "beach hut",
-  picture:"http://restaurantschico.com/beach-hut-deli",
+  },
+  { description: "Beach Hut",
+  picture:"http://restaurantschico.com/wp-content/uploads/2012/01/restaurants-chico-ca-beach-hut-deli.jpg",
   from:"restaurantschico.com"
-  }
-  { description: "bidwell mansion"
-  picture:"https://www.parks.ca.gov/?page_id=460",
-  from:"parks.ca.gov"
+  },
+  { description: "Bidwell Mansion",
+  picture:"http://c8.alamy.com/comp/BY16WE/elk238-x095-california-chico-bidwell-mansion-state-historic-park-1865-BY16WE.jpg",
+  from:"alamy.com"
   }];
 var current;
 function shuffleGallery()
@@ -28,5 +28,8 @@ function shuffleGallery()
       randomNumber= chico[Math.floor(Math.random()*chico.length)];
   }
   current = randomNumber;
+  document.getElementById("caption").innerHTML= current.description;
+  document.getElementById("picture").src= current.picture;
+  document.getElementById("source").innerHTML= "Image courtesy of: " + current.from
   console.log(randomNumber);
 }
