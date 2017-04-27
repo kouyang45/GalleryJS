5273b4491b158bbe45acc689ccbe4c9b104bd3f2
kou yang <kyang45@mail.csuchico.edu>
2017-04-27 12:50:44 -0700

    lab x24
    using tuesdays lab. look for images and use descricption for the name of place, picture for the url, and from for the source. all of this goes in var chico so the string will give all the info placed in string.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,32 @@
-
-
+var chico=[
+  { description: "bidwell park",
+  picture:"https://www.downtownchico.com/bidwell-park.htm",
+  from:"downtownchico.com"
+  }
+  { description: "bear hole",
+  picture:"https://www.pinterest.com/pin/446067538063953238/",
+  from:"pinterest.com"
+  }
+  { description: "observatory",
+  picture:"http://robinindar.com/portfolio/public-mosaic-art/chico-observatory-night-sky-mosaic/",
+  from:"robinindar.com"
+  }
+  { description: "beach hut",
+  picture:"http://restaurantschico.com/beach-hut-deli",
+  from:"restaurantschico.com"
+  }
+  { description: "bidwell mansion"
+  picture:"https://www.parks.ca.gov/?page_id=460",
+  from:"parks.ca.gov"
+  }];
+var current;
 function shuffleGallery()
 {
-
+  var randomNumber= chico[Math.floor(Math.random()*chico.length)];
+  while (current == randomNumber)
+  {
+      randomNumber= chico[Math.floor(Math.random()*chico.length)];
+  }
+  current = randomNumber;
+  console.log(randomNumber);
 }
